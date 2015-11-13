23a04a85110e1c3383a7d2cf72ca2a920e1066ec
Christopher Aldama Perez <radiomistc@gmail.com>
2015-11-12 23:23:39 -0600

    Pfin sibling

--- App/Html/js/index.js
+++ App/Html/js/index.js
@@ -1,72 +1,68 @@
     "use strict"
-    
+
     var bloques = document.getElementById("blocks");
     var programa = document.getElementById("program");
-    
+
     var drake = dragula([bloques, programa], {
       copy: true,
       accepts: function(el, target, source, sibling){
-         // console.log("accept: " + target.className);
-          if(el.className.indexOf("p-expr-log") !== -1
-            && target.className.indexOf("p-logica") === -1){
+          //console.log("accept: " + target.className);
+          if(sibling){
+            //console.log("si: " + sibling.className);
+            var type = sibling.getAttribute("tipo");
+            if(type == "begin"){
               return false;
             }
-          if (target.className.indexOf("blocks") !== -1
-                || !sibling
-                || sibling.className.indexOf("p-inicio") !== -1 ){
-              return false;              
           }
-          return true;
+          else {
 
+            //return false;
+          }
+
+          return true;
       },
       moves: dontMove
-  });  
+  });
   drake.on("drop", function(el, target ,source){
       //console.log(target.innerHTML);
-      if(target &&
-         target.className.indexOf("p-logica") !== -1){
-        removeTag("<br>", target);
-       //rmClass(el, "gu-transit");
-      }
-      
-      
+
       if(el.className.indexOf("p-repetir") !== -1){
           var mid = getChildByClassName(el, "p-repetir-mid");
-          drake.containers.push(mid);          
+          drake.containers.push(mid);
       }
         else if(el.className.indexOf("p-sino") !== -1){
           var mid1 = getChildByClassName(el, "p-sino-mid-1");
           var mid2 = getChildByClassName(el, "p-sino-mid-2");
           drake.containers.push(mid1);
-          drake.containers.push(mid2);          
+          drake.containers.push(mid2);
       }
       else if(el.className.indexOf("p-si") !== -1){
           var mid = getChildByClassName(el, "p-si-mid");
           var sup = getChildByClassName(el, "p-si-sup");
           var log = getChildByClassName(sup, "p-logica");
           drake.containers.push(log);
-          drake.containers.push(mid);          
+          drake.containers.push(mid);
 
-      }    
-      
+      }
+
   });
-    
 
-dragula([programa], {
+
+/*dragula([programa], {
      removeOnSpill: true,
      moves: dontMove
-   
-});
+
+});*/
 
 function dontMove(el, container, handle)
 {
          //console.log("moves:" + el.className);
          if( el.className.indexOf("p-inicio") !== -1
-             || el.className.indexOf("label") !== -1 
+             || el.className.indexOf("label") !== -1
              || el.className.indexOf("p-fin") !== -1){
              return false;
          }
-         return true;         
+         return true;
 }
 
 function getChildByClassName(el, className){
@@ -75,7 +71,7 @@ function getChildByClassName(el, className){
         if(childs[i].className &&
             childs[i].className.indexOf(className) !== -1){
             return childs[i];
-        }        
+        }
     }
 }
 
@@ -83,7 +79,7 @@ function removeTag(tag, el){
   var html = el.innerHTML.replace(new RegExp(tag, 'g' ), "");
   el.innerHTML = html;
   //console.log(html);
-  
+
 }
 
 function rmClass (el,  className) {
